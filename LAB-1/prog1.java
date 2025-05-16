f11b59d6a60253d3bb1e89e6abf443174e98bc1c
Jiya <jiyajha2020@gmail.com>
2025-05-17 01:39:58 +0530

    Create prog1.java

---
+++ LAB-1/prog1.java
@@ -0,0 +1,13 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Jiya";
+        String rollNo = "2205XXX";
+        String section = "A";
+        String branch = "CSE";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
